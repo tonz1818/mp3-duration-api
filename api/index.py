d6e06860afbdcc0b5bede73b59b525de8540baa7
Tony Sawlwin <tony@Lorettas-MacBook-Air.local>
2025-06-18 23:44:59 +0700

    v26

--- api/index.py
+++ api/index.py
@@ -5,12 +5,12 @@ class handler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers.get('Content-Length', 0))
         post_data = self.rfile.read(content_length)
-        print(post_data)
+        
 
         self.send_response(200)
         self.send_header('Content-type','application/json')
         self.end_headers()
-        message = '{"value": 55}'
+        message = '{"value": post_data}'
         self.wfile.write(message.encode())
         return
 
