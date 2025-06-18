a4a0ac7fa3b9092acf8fdb3e2caee796c17a8834
Tony Sawlwin <tony@Lorettas-MacBook-Air.local>
2025-06-18 23:24:21 +0700

    v24

--- api/index.py
+++ api/index.py
@@ -2,10 +2,22 @@ from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
+    def do_POST(self):
+        content_length = int(self.headers.get('Content-Length', 0))
+        post_data = self.rfile.read(content_length)
+        print(post_data)
+
         self.send_response(200)
         self.send_header('Content-type','application/json')
         self.end_headers()
         message = '{"value": 55}'
+        self.wfile.write(message.encode())
+        return
+
+    def do_GET(self):
+        self.send_response(400)
+        self.send_header('Content-type','application/json')
+        self.end_headers()
+        message = '{"error": "POST REQUEST ONLY"}'
         self.wfile.write(message.encode())
         return
